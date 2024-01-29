8cd3a5de092ad6b12834a9cf872f863e435e73dd
Amrita Pasupuleti <amritapasupuleti@gmail.com>
2024-01-29 00:07:15 -0800

    added navigation to subpages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 // App.js
-import { Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import Navbar from './Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Navbar from "./Navbar";
+//sub pages:
+import Checklist from "./SubPages/Checklist";
+import Inventory from "./SubPages/Inventory";
 
 const App = () => {
- return (
+  return (
     <div>
       <Navbar />
-       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-       </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/checklist" element={<Checklist />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
- );
+  );
 };
 
 export default App;
--- src/SubPages/Checklist.js
+++ src/SubPages/Checklist.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Checklist.css";
 
 const Checklist = () => {
-    return (
-        <div className="Checklist">
-            <h1>Checklist SHIATTT</h1>
-        </div>
-
-    );
+  return (
+    <div className="Checklist">
+      <h1>Checklist SHIATTT</h1>
+      <p>lol what are these names - Amrita</p>
+    </div>
+  );
 };
 export default Checklist;
